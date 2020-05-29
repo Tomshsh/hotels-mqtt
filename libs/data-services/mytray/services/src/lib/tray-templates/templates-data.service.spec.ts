bea1d2b0c4078d4040c9cc1e9c0ec01f0af9e840
hackp0int <genashm@gmail.com>
2020-05-30 00:23:53 +0300

    templates-data.service.spec.ts fix

--- libs/data-services/mytray/services/src/lib/tray-templates/templates-data.service.spec.ts
+++ libs/data-services/mytray/services/src/lib/tray-templates/templates-data.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TemplatesDataService } from './templates-data.service';
+import { TemplatesDataRepository } from '@my-tray/data-layers/mytray/repositories';
+import { AuthSessionQuery } from '@my-tray/shared/client/auth';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { environment } from '@my-tray/shared/utilities/mocks';
 
 describe('TemplatesDataService', () => {
   let service: TemplatesDataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const mockEnv =
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [
+          { provide: TemplatesDataRepository, useValue: {} },
+          { provide: AuthSessionQuery, useValue: {} },
+          { provide: HttpClient, useValue: {} },
+          { provide: 'env', useValue: environment }
+        ]
+      });
     service = TestBed.inject(TemplatesDataService);
   });
 
