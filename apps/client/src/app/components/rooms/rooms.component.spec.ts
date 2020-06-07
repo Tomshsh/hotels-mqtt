9681529a69220974c8d8da7f27c12c47b56a3ff6
hackp0int <genashm@gmail.com>
2020-06-07 14:17:10 +0300

    fix testing for rooms.component

--- apps/client/src/app/components/rooms/rooms.component.spec.ts
+++ apps/client/src/app/components/rooms/rooms.component.spec.ts
@@ -1,16 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RoomsComponent } from './rooms.component';
+import { NbDialogService, NbToastrService } from '@nebular/theme';
+import { ChangeDetectorRef, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RoomDataService } from '@my-tray/data-services/mytray/services';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthSessionQuery } from '@my-tray/shared/client/auth';
+import { environment } from '@my-tray/shared/utilities/mocks';
 
 describe('RoomsComponent', () => {
   let component: RoomsComponent;
   let fixture: ComponentFixture<RoomsComponent>;
 
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RoomsComponent ]
+      imports: [HttpClientTestingModule],
+      declarations: [RoomsComponent],
+      providers: [
+        RoomDataService,
+        { provide: NbDialogService, useValue: {} },
+        { provide: NbToastrService, useValue: {} },
+        ChangeDetectorRef,
+        AuthSessionQuery,
+        {
+          provide: 'env',
+          useValue: environment
+        }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
