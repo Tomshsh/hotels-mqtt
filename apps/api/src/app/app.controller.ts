dd941d1035bc631204dbf20833074a5158a860fb
Tomshsh <shmtomer@gmail.com>
2020-08-23 09:38:57 +0300

    -

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,43 +1,55 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import Parse from 'parse/node'
-import {StaffAlertService} from '@my-tray/data-services/api'
+import { StaffAlertService } from '@my-tray/data-services/api'
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    // private staffAlertService: StaffAlertService
-    ) {}
+    private staffAlertService: StaffAlertService
+  ) { }
 
   @Get('hello')
-  getData(){
-    return this.appService.getData();
+  getData() {
+    return this.staffAlertService.alertStaff('+972503028023')
+    // return this.appService.getData();
   }
 
   @Post('towels')
-  drawTowels(@Body() {cardNum, quantity}: {cardNum: string, quantity: number} ){
+  drawTowels(@Body() { cardNum, quantity }: { cardNum: string, quantity: number }) {
     const q = new Parse.Query('RoomTowels')
     q.equalTo('cards', cardNum)
     return q.first()
-    .then(rt => {
-      if(!(rt.get('towelLimit') - rt.get('currCount') - quantity < 0)){
-        const q = new Parse.Query('Locker')
-        q.greaterThan('quantity', quantity)
-        return Promise.all([q.first(), rt])
-      }else throw('dsdssd')
-    })
-    .then(([locker, rt]) => {
-      if(locker){
-        locker.increment('quantity',-quantity),
-        rt.increment('currCount',quantity)
-        return Promise.all([
-          rt.save(),
-          locker.save()
-        ])
-      }
-    })
-    .then(([rt, locker]) => ({rt, locker}))
-    .catch((err) => console.log("err"))
+      .then(rt => {
+        if (!(rt.get('towelLimit') - rt.get('currCount') - quantity < 0)) {
+          const q = new Parse.Query('Locker')
+          q.greaterThan('quantity', quantity)
+          return Promise.all([q.first(), rt])
+        } else throw ({ message: 'reached limit for guest'})
+      })
+      .then(([locker, rt]) => {
+        if (locker) {
+          locker.increment('quantity', -quantity),
+            rt.increment('currCount', quantity)
+          return Promise.all([
+            rt.save(),
+            locker.save()
+          ])
+        } else throw ({
+          message: 'no lockers met quantity requirements',
+          action: () => {
+            const q = new Parse.Query('Maintenance')
+            q.equalTo('chores', 'refill lockers')
+            q.equalTo('onShift', true)
+            q.find()
+          }
+        })
+      })
+      .then(([rt, locker]) => ({ rt, locker }))
+      .catch((err) => {
+        console.log(err.message)
+
+      })
   }
 }
--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -6,14 +6,6 @@ import { BackendModule, ConfigurationService } from '@my-tray/shared/backend'
 import {environment} from '../environments'
 import {ApiModule} from '@my-tray/data-services/api'
 
-// client.messages
-//   .create({
-//      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//      from: '+13157374243',
-//      to: '+972503028023'
-//    })
-//   .then(message => console.log(message.sid));
-
 export function initConfig(configurationService: ConfigurationService) {
   configurationService.initializeConfiguration(environment);
 }
