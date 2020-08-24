3d75413a4209b4f967c811a26eb0893752ed7dd2
Tomshsh <shmtomer@gmail.com>
2020-08-24 17:01:29 +0300

    template for interval to run locker status check once an hour

--- libs/data-services/api/src/api.module.ts
+++ libs/data-services/api/src/api.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ApiService } from './api.service';
-import { StaffAlertService } from './services';
+import { StaffAlertService, TowelsService, MaintanenceService, LockersService, TaskService } from './services';
 import { BackendModule } from '@my-tray/shared/backend';
-import { TowelsService } from './services';
-import { MaintanenceService } from './services';
-import { LockersService } from './services';
 
 @Module({
   imports: [BackendModule, ],
-  providers: [ApiService, StaffAlertService, TowelsService, MaintanenceService, LockersService],
+  providers: [ApiService, StaffAlertService, TowelsService, MaintanenceService, LockersService, TaskService],
   exports: [ApiService, StaffAlertService, TowelsService, MaintanenceService, LockersService],
 })
 export class ApiModule { }
--- libs/data-services/api/src/services/index.ts
+++ libs/data-services/api/src/services/index.ts
@@ -2,3 +2,4 @@ export * from './staff-alert';
 export * from './towels';
 export * from './maintanence';
 export * from './lockers';
+export * from './task';
