0c6ee145cce5ad22bead7dfba90ee6c267d2d867
Tomshsh <shmtomer@gmail.com>
2020-10-28 18:06:12 +0200

    minibars.service complies with new minibars db structure

--- libs/data-services/api/src/services/minibars/minibars.service.ts
+++ libs/data-services/api/src/services/minibars/minibars.service.ts
@@ -10,26 +10,40 @@ export class MinibarsService {
   private findMinibar(deviceId) {
     return new Parse.Query("Minibar")
       .include('room')
+      .include('state.tag.product')
       .get(deviceId, { sessionToken: this.configService.user.getSessionToken() });
   }
 
   async findProductPrice(itemType) {
     const sessionToken = this.configService.user.getSessionToken()
     const product = await new Parse.Query('Product')
-      .equalTo('shortName', itemType)
+      .equalTo('title', itemType)
       .first({ sessionToken })
     return product.get('price')
   }
 
   async get(deviceId, itemType) {
     const minibar = await this.findMinibar(deviceId)
-    minibar.decrement(itemType, 1)
-    return minibar.save(null, { sessionToken: this.configService.user.getSessionToken() })
+    const state: any[] = minibar.get("state")
+    const index = state.findIndex(s => s.tag.get('product').get('title') == itemType)
+    if (index > -1) {
+      state.splice(index, 1)
+      minibar.set('state', state)
+      return minibar.save(null, { sessionToken: this.configService.user.getSessionToken() })
+    } else { throw new Error('product with title: ' + itemType + ' was not found') }
   }
 
   async put(deviceId, itemType) {
+    const sessionToken = this.configService.user.getSessionToken()
     const minibar = await this.findMinibar(deviceId)
-    minibar.increment(itemType, 1)
-    return minibar.save(null, { sessionToken: this.configService.user.getSessionToken() })
+    const innerQuery = new Parse.Query('Product').equalTo('title', itemType)
+    const tag = await new Parse.Query('Tag').matchesQuery('product', innerQuery).first({ sessionToken })
+    const newObj = {
+      tag: tag.toPointer(),
+      lastAction: 'refill',
+      updatedAt: { type: '__date', iso: new Date() }
+    }
+    minibar.add('state', newObj)
+    return minibar.save(null, { sessionToken })
   }
 }
